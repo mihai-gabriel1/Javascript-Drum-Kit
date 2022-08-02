1e641cc607126c1b170934e8686b055a2b2425b4
mihai-gabriel1 <mr.eu21@gmail.com>
2022-08-02 13:13:39 +0300

    Work has been bone towards the files

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+window.addEventListener("keydown", function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"`);
+  if (!audio) return; //Pause all the functions from running;
+  audio.curentTime = 0; //rewind to the start !
+  audio.play();
+  key.classList.add("playing");
+});
